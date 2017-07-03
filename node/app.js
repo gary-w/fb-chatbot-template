18434c2e543a52a58cfdc5a38c06db5577596fd2
Gary Wong <gary.wonghg@gmail.com>
2017-07-03 22:02:18 +0800

    (init) Test root route

--- node/app.js
+++ node/app.js
@@ -9,6 +9,12 @@ app.set( "port", process.env.PORT || 1337 );
 // app.use( bodyParser.json( { verify: verifyRequestSignature } ) );
 app.use( express.static( "public" ) );
 
+// TODO: Error handling if not 200
+app.get( "/", ( req, res ) => {
+    res.status( 200 )
+       .send( "This is a Fb chatbot." );
+} );
+
 app.listen( app.get( "port" ), () => {
     console.log( "Fb chatbot is running on port", app.get( "port" ) );
 } );
