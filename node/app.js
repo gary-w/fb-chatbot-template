46e5459e8389e1b5fe8907f259ee0bb2b58f5495
Gary Wong <gary.wonghg@gmail.com>
2017-07-03 22:38:27 +0800

    (auth) Set APP_SECRET, VALID_TKN, PG_ACCESS_TKN, SERVER_URL

--- node/app.js
+++ node/app.js
@@ -1,6 +1,7 @@
 const express = require( "express" );
 const request = require( "request" );
 const bodyParser = require( "body-parser" );
+const config = require( "config" );
 
 const app = express();
 
@@ -8,6 +9,31 @@ app.set( "port", process.env.PORT || 1337 );
 // app.set( "view engine", "ejs" );
 // app.use( bodyParser.json( { verify: verifyRequestSignature } ) );
 app.use( express.static( "public" ) );
+
+const APP_SECRET = ( process.env.APP_SECRET ) ?
+  process.env.APP_SECRET :
+  config.get( "appSecret" );
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = ( process.env.VALIDATION_TOKEN ) ?
+  ( process.env.VALIDATION_TOKEN ) :
+  config.get( "validationToken" );
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = ( process.env.PAGE_ACCESS_TOKEN ) ?
+  ( process.env.PAGE_ACCESS_TOKEN ) :
+  config.get( "pageAccessToken" );
+
+// URL where the app is running (include protocol). Used to point to scripts and
+// assets located at this address.
+const SERVER_URL = ( process.env.SERVER_URL ) ?
+  ( process.env.SERVER_URL ) :
+  config.get( "serverURL" );
+
+if ( !( APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL ) ) {
+    console.error( "Missing config values" );
+    process.exit( 1 );
+}
 
 // TODO: Error handling if not 200
 app.get( "/", ( req, res ) => {
