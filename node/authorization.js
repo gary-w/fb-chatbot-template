705bab5a64dc8ca4f7b376d578166ece8941dd2e
Gary Wong <gary.wonghg@gmail.com>
2017-07-04 13:14:33 +0800

    (fix) Reverse async await

--- node/authorization.js
+++ node/authorization.js
@@ -1,25 +1,21 @@
 const keys = require( "./keys" );
 
-const verifyRequestSignature = async ( req, res, buf ) => {
-    try {
-        const signature = req.headers[ "x-hub-signature" ];
+const verifyRequestSignature = ( req, res, buf ) => {
+    const signature = req.headers[ "x-hub-signature" ];
 
-        if ( !signature ) {
-            console.error( "Couldn't validate the signature." );
-        } else {
-            const elements = signature.split( "=" );
-            const signatureHash = elements[ 1 ];
+    if ( !signature ) {
+        console.error( "Couldn't validate the signature." );
+    } else {
+        const elements = signature.split( "=" );
+        const signatureHash = elements[ 1 ];
 
-            const expectedHash = crypto.createHmac( "sha1", await keys.APP_SECRET )
-                            .update( buf )
-                            .digest( "hex" );
+        const expectedHash = crypto.createHmac( "sha1", keys.APP_SECRET )
+                        .update( buf )
+                        .digest( "hex" );
 
-            if ( signatureHash !== expectedHash ) {
-                throw new Error( "Couldn't validate the request signature." );
-            }
+        if ( signatureHash !== expectedHash ) {
+            throw new Error( "Couldn't validate the request signature." );
         }
-    } catch ( err ) {
-        res.status( 500 ).send( err.message );
     }
 };
 
--- node/request_handlers.js
+++ node/request_handlers.js
@@ -3,41 +3,34 @@ const events = require( "./messageEvents" );
 
 const VALIDATION_TOKEN = keys.VALIDATION_TOKEN;
 
-const validatePageAndToken = async ( req, res ) => {
-    try {
-        if ( req.query[ "hub.mode" ] === "subscribe" &&
-        req.query[ "hub.verify_token" ] === VALIDATION_TOKEN ) {
-            console.log( "Validating webhook" );
-            res.status( 200 ).send( req.query[ "hub.challenge" ] );
-        } else {
-            console.error( "Failed validation. Make sure the validation tokens match." );
-        }
-    } catch ( err ) {
-        res.status( 403 ).send( err.message );
+const validatePageAndToken = ( req, res ) => {
+    if ( req.query[ "hub.mode" ] === "subscribe" &&
+    req.query[ "hub.verify_token" ] === VALIDATION_TOKEN ) {
+        console.log( "Validating webhook" );
+        res.status( 200 ).send( req.query[ "hub.challenge" ] );
+    } else {
+        console.error( "Failed validation. Make sure the validation tokens match." );
     }
 };
 
-const routeMessageEvents = async ( req, res ) => {
+const routeMessageEvents = ( req, res ) => {
     const data = req.body;
-    try {
-        if ( data.object === "page" ) {
-            data.entry.forEach( ( pageEntry ) => {
-                // TODO: Check if the message is intended for the page
-                pageEntry.messaging.forEach( ( messagingEvent ) => {
-                    if ( messagingEvent.message ) {
-                        events.receivedMessage( messagingEvent );
-                    } else if ( messagingEvent.postback ) {
-                        events.receivedPostback( messagingEvent );
-                    } else {
-                        console.log( "Webhook received unknown messagingEvent: ", messagingEvent );
-                    }
-                } );
-            } );
 
-            res.sendStatus( 200 ); // send back a 200 within 20 seconds.
-        }
-    } catch ( err ) {
-        res.status( 500 ).send( err.message );
+    if ( data.object === "page" ) {
+        data.entry.forEach( ( pageEntry ) => {
+            // TODO: Check if the message is intended for the page
+            pageEntry.messaging.forEach( ( messagingEvent ) => {
+                if ( messagingEvent.message ) {
+                    events.receivedMessage( messagingEvent );
+                } else if ( messagingEvent.postback ) {
+                    events.receivedPostback( messagingEvent );
+                } else {
+                    console.log( "Webhook received unknown messagingEvent: ", messagingEvent );
+                }
+            } );
+        } );
+
+        res.sendStatus( 200 ); // send back a 200 within 20 seconds.
     }
 };
 
