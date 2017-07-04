c3d1ad62862a167d54604723a767694158cd599c
Gary Wong <gary.wonghg@gmail.com>
2017-07-04 12:17:26 +0800

    (refactor) Modularize request handlers for /fb_webhook

--- node/app.js
+++ node/app.js
@@ -3,9 +3,8 @@ const bodyParser = require( "body-parser" );
 const dotenv = require( "dotenv" );
 const path = require( "path" );
 
-const keys = require( "./keys.js" );
 const auth = require( "./authorization" );
-const events = require( "./messageEvents" );
+const rh = require( "./request_handlers" );
 
 const app = express();
 
@@ -18,42 +17,12 @@ app.use( bodyParser.json() );
 app.use( bodyParser.json( { verify: auth.verifyRequestSignature } ) );
 app.use( express.static( "public" ) );
 
-const VALIDATION_TOKEN = keys.VALIDATION_TOKEN;
+app.route( "/fb_webhook" )
+    .get( rh.validatePageAndToken )
+    .post( rh.routeMessageEvents );
 
 app.get( "/", ( req, res ) => {
     res.status( 200 ).send( "This is a Fb chatbot." );
-} );
-
-app.get( "/fb_webhook", ( req, res ) => {
-    if ( req.query[ "hub.mode" ] === "subscribe" &&
-      req.query[ "hub.verify_token" ] === VALIDATION_TOKEN ) {
-        console.log( "Validating webhook" );
-        res.status( 200 ).send( req.query[ "hub.challenge" ] );
-    } else {
-        console.error( "Failed validation. Make sure the validation tokens match." );
-        res.sendStatus( 403 );
-    }
-} );
-
-app.post( "/fb_webhook", ( req, res ) => {
-    const data = req.body;
-
-    if ( data.object === "page" ) {
-        data.entry.forEach( ( pageEntry ) => {
-            // TODO: Check if the message is intended for the page
-            pageEntry.messaging.forEach( ( messagingEvent ) => {
-                if ( messagingEvent.message ) {
-                    events.receivedMessage( messagingEvent );
-                } else if ( messagingEvent.postback ) {
-                    events.receivedPostback( messagingEvent );
-                } else {
-                    console.log( "Webhook received unknown messagingEvent: ", messagingEvent );
-                }
-            } );
-        } );
-
-        res.sendStatus( 200 ); // send back a 200 within 20 seconds.
-    }
 } );
 
 app.listen( app.get( "port" ), () => {
---
+++ node/request_handlers.js
@@ -0,0 +1,41 @@
+const keys = require( "./keys" );
+const events = require( "./messageEvents" );
+
+const VALIDATION_TOKEN = keys.VALIDATION_TOKEN;
+
+const validatePageAndToken = ( req, res ) => {
+    if ( req.query[ "hub.mode" ] === "subscribe" &&
+      req.query[ "hub.verify_token" ] === VALIDATION_TOKEN ) {
+        console.log( "Validating webhook" );
+        res.status( 200 ).send( req.query[ "hub.challenge" ] );
+    } else {
+        console.error( "Failed validation. Make sure the validation tokens match." );
+        res.sendStatus( 403 );
+    }
+};
+
+const routeMessageEvents = ( req, res ) => {
+    const data = req.body;
+
+    if ( data.object === "page" ) {
+        data.entry.forEach( ( pageEntry ) => {
+            // TODO: Check if the message is intended for the page
+            pageEntry.messaging.forEach( ( messagingEvent ) => {
+                if ( messagingEvent.message ) {
+                    events.receivedMessage( messagingEvent );
+                } else if ( messagingEvent.postback ) {
+                    events.receivedPostback( messagingEvent );
+                } else {
+                    console.log( "Webhook received unknown messagingEvent: ", messagingEvent );
+                }
+            } );
+        } );
+
+        res.sendStatus( 200 ); // send back a 200 within 20 seconds.
+    }
+};
+
+module.exports = {
+    validatePageAndToken,
+    routeMessageEvents,
+};
